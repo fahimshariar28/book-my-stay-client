5e9593e2153524d84aa9d8de7fcd94f2290e44f0
Fahim Shariar <fahimshariar28@gmail.com>
2023-08-26 22:23:28 +0600

    useFetch hook updated

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,12 +1,37 @@
-import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const useFetch = (url) => {
-  const { data, error, loading, refetch } = useQuery({
-    queryKey: [url],
-    queryFn: () => axios.get(url).then((res) => res.data),
-  });
-  return { data, error, loading, refetch };
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetch };
 };
 
 export default useFetch;
