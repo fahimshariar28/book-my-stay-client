bf76bb79e2c8c13c52116603a94697c80ef81842
Fahim Shariar <fahimshariar28@gmail.com>
2023-08-26 18:12:22 +0600

    Tanstackquery added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const query = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={query}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
